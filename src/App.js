d732ab0f2bcf9f8f72760528e3c08591c35fe59d
Raditya0902 <aditya.rallapalli0902@gmail.com>
2023-08-03 15:02:07 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import LoadingSpinner from "./LoadingSpinner";
+import "./index.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleFetch = () => {
+    setIsLoading(true);
+    fetch("https://reqres.in/api/users?page=1")
+      .then((response) => response.json())
+      .then((response) => {
+        // setUsers(response.data);
+        // setIsLoading(false);
+        setTimeout(() => {
+          setUsers(response.data);
+          setIsLoading(false);
+        }, 2000);
+      })
+      .catch(() => {
+        setErrorMessage("Unable to fetch users info.");
+        setIsLoading(false);
+      });
+  };
+
+  const renderUser = (
+    <div className="users-container">
+      {users.map((user, index) => (
+        <div className="card" key={index}>
+          <img
+            className="avatar"
+            src={user.avatar}
+            alt={`${user.first_name} ${user.last_name}`}
+          />
+          <div className="data">
+            <div>
+              <h3>{`${user.first_name} ${user.last_name}`} </h3>
+              <a href={`mailto:${user.email}`}>{user.email}</a>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
+  );
+
+  return (
+    <>
+      <header className="header">
+        <h1 className="logo">Users info</h1>
+        <button onClick={() => handleFetch()} disabled={isLoading}>
+          Get Info
+        </button>
+      </header>
+      {isLoading ? <LoadingSpinner /> : renderUser}
+      {errorMessage && <div className="error">{errorMessage}</div>}
+    </>
   );
 }
 
